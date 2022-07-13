18daa5a0c052171b5154e741cfb323c50cec2592
mykmicheals <mykmicheals@gmail.com>
2022-07-13 15:15:51 +0100

    initial-commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+      <div className='banner'>
+          <h1>SOMETHING AWESOME IS COMMING SOON</h1>
+    </div>
+  )
+}
+
+export default HomeScreen
