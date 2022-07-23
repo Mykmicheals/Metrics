744e4e47fdb1f3c4975d5b22a5ac571ebdb28cc9
mykmicheals <mykmicheals@gmail.com>
2022-07-23 20:59:34 +0100

    responsive-styles

--- src/Screens/BlogScreen/BlogEach.js
+++ src/Screens/BlogScreen/BlogEach.js
@@ -6,10 +6,15 @@ import manImage from '../../assets/images/work.png'
 
 function BlogEach() {
     return (
-        <div className='blog-bottom'>
-            <h3>Articles and News</h3>
-            <small>Blog</small>
-            <i id='arrow-icon'><Icon icon="bi:arrow-right" color="#d105d4" inline={true} /></i>
+        <div>
+            <div className='blog-bottom'>
+                <h3>Articles and News</h3>
+                <small>Blog</small>
+                <i id='arrow-icon'><Icon icon="bi:arrow-right" color="#d105d4" inline={true} /></i>
+            </div>
+            <div className='blog-details-image-section'>
+                <img src={manImage} alt='blog-image' />
+            </div>
         </div>
     )
 }
